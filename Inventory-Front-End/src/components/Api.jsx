bf0223477bcb9a695e38a469f7a440e8ea6a7c7d
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-04-02 22:26:39 +0100

    artists API

--- Inventory-Front-End/src/components/Api.jsx
+++ Inventory-Front-End/src/components/Api.jsx
@@ -1,2 +1,16 @@
 import axios from "axios";
 
+//artists API
+ export const getArtists = async () => {
+    const { 
+        data: { results },
+    } = await axios.get("http://localhost:5174/api/artists");
+
+    const artistsData = await Promise.all(
+      results.map(async ({ url }) => {
+        const { data } = await axios.get(url);
+        return data;
+    })
+  );
+    return artistsDatas;
+  };
