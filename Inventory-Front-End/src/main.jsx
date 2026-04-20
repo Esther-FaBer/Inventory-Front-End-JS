8d631e0e21b99b406dff1f4673a8b1b347d1aa06
Esther-FaBer <165953395+Esther-FaBer@users.noreply.github.com>
2026-04-20 22:27:45 +0100

    app

--- Inventory-Front-End/src/main.jsx
+++ Inventory-Front-End/src/main.jsx
@@ -1,13 +1,7 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { AppProviders } from './context';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AppProviders>
-      <App />
-    </AppProviders>
-  </StrictMode>,
-);
+<BrowserRouter>
+  <AppProviders>
+    <App />
+  </AppProviders>
+</BrowserRouter>
